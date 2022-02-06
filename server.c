5429ba6249e6c6b588a76fe79606fd1650fe2b33
yili <yili.ng@mail.utoronto.ca>
2022-02-05 21:07:42 -0500

    complete section 2.2 server

--- server.c
+++ server.c
@@ -1,30 +1,82 @@
 #include <stdio.h>
 #include <string.h>
-#include <sys/types.h>
+#include <stdlib.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <arpa/inet.h>
+#include "packet.h"
 
-int main() {
+#define BUFSIZE 1100
+
+struct packet stringToPacket(char * buffer){
+    char * current_char = buffer;
+    struct packet recv_packet = {0};
+    
+    char total_frag[4] = "";  
+    while(current_char[0] != ':'){
+        strncat(total_frag,current_char,1);
+        current_char += 1;
+    }
+    recv_packet.total_frag = atoi(total_frag);
+    current_char += 1;
+
+    char frag_no[4] = "";
+    while(current_char[0] != ':'){
+        strncat(frag_no,current_char,1);
+        current_char += 1;
+    }
+    recv_packet.frag_no = atoi(frag_no);
+    current_char += 1;
+
+    char size[4] = "";
+    while(current_char[0] != ':'){
+        strncat(size,current_char,1);
+        current_char += 1;
+    }
+    recv_packet.size = atoi(size);
+    current_char += 1;
+
+    char filename[100] = "";
+    while(current_char[0] != ':'){
+        strncat(filename,current_char,1);
+        current_char += 1;
+    }
+    recv_packet.filename = filename;
+    current_char += 1;
+
+    for(int i = 0; i < recv_packet.size; i++){
+        recv_packet.filedata[i] = *current_char;
+        current_char += 1;
+    }
+
+    return recv_packet;
+}
+
+int main(int argc, char *argv[]) {
     printf("Hello from server!\n");
-    printf("Put in command of this form (port number > 1024)\n");
-    printf("     ftp <UDP listen port>\n");
 
-    // take in user input - port number
-    int user_input;
-    char ftp[3];
-
-    scanf("%s %d", ftp, &user_input);
-    if(user_input < 1024 && user_input > 65535){
+    // check number of arguments
+    if(argc != 2){
+        fprintf(stderr,"Make sure to use: server <UDP listen port> (port number > 1024)\n");
+        return 0;
+    }
+  
+    // take in user input 
+    char * ptr;
+    int port_num = strtol(argv[1], &ptr , 10);
+    printf("%d\n", port_num);
+    if(port_num < 1024 && port_num > 65535){
         printf("port number invalid\n");
         return 0;
     }
-    //open UDP socket
+
+    // open UDP socket
     int udp_fd = socket(AF_INET, SOCK_DGRAM, 0);
-    //printf("fd %d %d\n", udp_fd, user_input);
+    //printf("fd %d %d\n", udp_fd, port_num);
 
     struct sockaddr_in server_addr= {0};   //server host & IP address
     server_addr.sin_family = AF_INET;  //problem here!
-    server_addr.sin_port = htons(user_input);
+    server_addr.sin_port = htons(port_num);
     server_addr.sin_addr = (struct in_addr) { htonl(INADDR_ANY) };
 
     int bind_result = bind(udp_fd, (struct sockaddr *) &server_addr, sizeof(server_addr) );
@@ -34,7 +86,7 @@ int main() {
     }
 
     //liten to port
-    char receive_buf[10];
+    char receive_buf[BUFSIZE];
     struct sockaddr_storage client_addr= {0};     //client host & IP address
     socklen_t clientAddrLen = sizeof(client_addr);
 
@@ -45,15 +97,16 @@ int main() {
 
         if(bytes_received > 0){
             //printf("buff %s\n", receive_buf);
+            struct packet receive_pack = stringToPacket(receive_buf);
 
             //Reply client
             char* message = "";
-            if (strncmp(receive_buf, "ftp", 3) == 0){   //not read as true
-                message = "yes\n";
+            if (receive_pack.total_frag > 0){
+                message = "yes\n";       //success
             }else{
                 message = "no\n";
             }
-
+            
             int size = strlen(message) + 1;
             ssize_t bytes_sent = 0;
             bytes_sent = sendto(udp_fd, message, size, 0, (struct sockaddr*) &client_addr, clientAddrLen);
@@ -62,6 +115,17 @@ int main() {
                 printf("fail to send yes/no reply to client\n");
             }else{
                 printf("client successfully replied\n");
+            }
+            
+            //Copy data into new file
+            FILE* file_ptr = fopen(receive_pack.filename, "a");
+
+            if(file_ptr != NULL){
+                fprintf(file_ptr, receive_pack.filedata);
+            }
+
+            if(receive_pack.frag_no = receive_pack.total_frag){
+                fclose(file_ptr);
             }
         }
     }
