4b54d2acf094472abab4053394ed4762494906ee
yili <yili.ng@mail.utoronto.ca>
2022-03-21 17:06:23 -0400

    shit yo

--- Lab-2/server.c
+++ Lab-2/server.c
@@ -42,13 +42,13 @@ Account accounts_db[NUM_ACC] = {{"a","aa"},{"b","bb"},{"c","cc"},{"d","dd"},
 
 
 typedef struct client {
+    char* name;
     char* session_id;  
     int socket_fd;
     bool isActive;
-    struct sockaddr_storage addr;   //IP and port number of client
 } Client;
 
-Client users_db[NUM_ACC] ;
+Client users_db[NUM_ACC];
 
 typedef struct session {
     char* session_id;
@@ -86,6 +86,15 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
+    //make user database
+    
+    for(int i=0; i < NUM_ACC; i++){
+        users_db[i].name = NULL;
+        users_db[i].session_id = NULL;
+        users_db[i].socket_fd = -1;
+        users_db[i].isActive = false;
+    }
+
     int tcp_socket = socket(AF_INET, SOCK_STREAM, 0);
 
     struct sockaddr_in server_addr= {0};   //server host & IP address
@@ -115,68 +124,71 @@ int main(int argc, char *argv[]) {
     //accept:
     struct sockaddr_storage client_addr= {0};     //client host & IP address
     socklen_t clientAddrLen = sizeof(client_addr);
-    int client_fd = accept(tcp_socket, (struct sockaddr*) &client_addr, &clientAddrLen);
-    printf("client fd is %d \n", client_fd); // -----------CHECK fd
 
-    //might have to do polling to accept next packet in queue
+    while(1){
+        int client_fd = accept(tcp_socket, (struct sockaddr*) &client_addr, &clientAddrLen);
+        printf("client fd is %d \n", client_fd); // -----------CHECK fd
 
-    char host[1024];
-    char service[20];
-    //change 0 to NI_NOFQDN
-    getnameinfo((struct sockaddr*) &client_addr, clientAddrLen, host, sizeof(host), service, sizeof(service), 0);  
-    printf(" client %s connected\n", host); // e.g. "www.example.com"
+        //might have to do polling to accept next packet in queue
+
+        char host[1024];
+        char service[20];
+        //change 0 to NI_NOFQDN
+        getnameinfo((struct sockaddr*) &client_addr, clientAddrLen, host, sizeof(host), service, sizeof(service), 0);  
+        printf(" client %s connected\n", host); // e.g. "www.example.com"
 
 
-    //Extract info from the accepted packet
-    //void * recv_buff;
-    char recv_buff[1000]; //------------------------------- MODIFIED
+        //Extract info from the accepted packet
+        //void * recv_buff;
+        char recv_buff[1000]; //------------------------------- MODIFIED
 
-    int num_chars = recv(client_fd, recv_buff, 1000, 0);
-    if(num_chars == 0){
-        printf("client closed connection on you\n");
-    }
-    if(num_chars < 0) {
-        printf("problem with recv()\n");
-        return -1;
-    }else{
-        printf("recevied amount %d", num_chars);
-    }
+        int num_chars = recv(client_fd, recv_buff, 1000, 0);
+        if(num_chars == 0){
+            printf("client closed connection on you\n");
+        }
+        if(num_chars < 0) {
+            printf("problem with recv()\n");
+            return -1;
+        }else{
+            printf("recevied amount %d", num_chars);
+        }
 
-    //extract info (type,data) out from recv_buff
-    struct message recv_packet = stringToPacket(recv_buff);
-    
-    switch (recv_packet.type)
-    {   
-        case 0: //login
-            login(recv_packet, client_fd);
-            break;
+        //extract info (type,data) out from recv_buff
+        struct message recv_packet = stringToPacket(recv_buff);
+        
+        switch (recv_packet.type)
+        {   
+            case 0: //login
+                login(recv_packet, client_fd);
+                break;
 
-        case 3: //exit
-            exit_conf(recv_packet, client_fd);
-            break;
-        case 4: //join
-            join(recv_packet, client_fd);
-            break;
-        case 7: //leave
-            leave_sess(recv_packet, client_fd);
-            break;
-        case 8: //new
-            new_sess(recv_packet, client_fd);
-            break;
-        case 10: //message
-            broadcast(recv_packet, client_fd);
-            break;
-        case 11: //query
-            getActiveUserSessions(recv_packet, client_fd);
-            break;
-    }
+            case 3: //exit
+                exit_conf(recv_packet, client_fd);
+                break;
+            case 4: //join
+                join(recv_packet, client_fd);
+                break;
+            case 7: //leave
+                leave_sess(recv_packet, client_fd);
+                break;
+            case 8: //new
+                new_sess(recv_packet, client_fd);
+                break;
+            case 10: //message
+                broadcast(recv_packet, client_fd);
+                break;
+            case 11: //query
+                getActiveUserSessions(recv_packet, client_fd);
+                break;
+        }
 
-    void* ack = "1:"; //acknowledgement packet
-    int len = strlen(ack);
-    //send: to client
-    int send_res = send(client_fd, ack, len, 0);
-    if(send_res < 0){
-        return -1;
+        void* ack = "1:"; //acknowledgement packet
+        int len = strlen(ack);
+        //send: to client
+        int send_res = send(client_fd, ack, len, 0);
+        if(send_res < 0){
+            return -1;
+        }
     }
 
     
@@ -193,11 +205,24 @@ int main(int argc, char *argv[]) {
 int login(struct message packet, int receiver_fd){
     struct message ack_pack = {0};
     strcpy(ack_pack.source, packet.source);
-    printf("!! %s == %s", accounts_db[0].name, packet.source);
-    for(int i=0; i < NUM_ACC; i++){
+    //printf("!! %s == %s", accounts_db[0].name, packet.source);
 
-        if(strcmp(accounts_db[i].name, packet.source) == 0){
-            if(strcmp(accounts_db[i].password, packet.data) == 0){
+    FILE* accs;
+    char line[10];
+
+    accs = fopen("accounts.txt", "r");
+    int res;
+
+    while(res != EOF || res == 0){
+        
+        res = fscanf(accs,"%s", line);
+        printf("%s\n", line); 
+        if(strstr(line, packet.source) != NULL ){ //user exists
+            int i = (int)line[0] - (int)'a'; 
+            printf("index %d", index);
+
+            //check password
+            if(strstr(line, packet.data) != NULL ){
                 if(users_db[i].isActive == false){
                     users_db[i].isActive = true;
 
@@ -216,6 +241,7 @@ int login(struct message packet, int receiver_fd){
                     return -1;
                 }
             }else{
+                //password is wrong
                 //send LO_NACK
                 ack_pack.type = 2;
                 strcpy(ack_pack.data, "wrong password");
@@ -223,9 +249,10 @@ int login(struct message packet, int receiver_fd){
                 sendPacket(ack_pack, receiver_fd);
                 return -1;
             }
-        }
+            
+        }  
     }
-
+    
     //send LO_NACK
     ack_pack.type = 2;
     strcpy(ack_pack.data, "user account not found");
@@ -244,6 +271,22 @@ int exit_conf(struct message packet, int receiver_fd){
     for(int i=0; i < NUM_ACC; i++){
         if(strcmp(accounts_db[i].name, packet.source) == 0){
             users_db[i].isActive = false;
+        }
+    }
+
+    FILE* accs;
+    char line[10];
+
+    accs = fopen("accounts.txt", "r");
+    int res;
+
+    while(res != EOF || res == 0){
+        
+        res = fscanf(accs,"%s", line);
+        printf("%s\n", line); 
+        if(strstr(line, packet.source) != NULL ){ //user exists
+            int i = (int)line[0] - (int)'a'; 
+            printf("index %d", index);
         }
     }
 }
@@ -449,7 +492,7 @@ struct message stringToPacket(char * buffer){
     recv_packet.size = atoi(size);
     current_char += 1;
 
-    printf("packet type %d, size %d\n", recv_packet.type, recv_packet.size);
+    printf("\npacket type %d, size %d\n", recv_packet.type, recv_packet.size);
 
     char source[1] = {0};
     while(current_char[0] != ':'){
